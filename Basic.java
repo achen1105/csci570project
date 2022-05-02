f8d00aaf883e994fd5fb0ad7afe8636b94d35495
achen1105 <achen1105@gmail.com>
2022-05-02 16:44:28 -0700

    compiling?

--- Basic.java
+++ Basic.java
@@ -1,4 +1,5 @@
 import java.io.File;
+import java.io.FileWriter;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Scanner;
@@ -14,6 +15,8 @@ public class Basic {
     // instance variables
     private String s1; // sequence 1
     private String s2; // sequence 2
+    private String a1; // alignment 1
+    private String a2; // alignment 2
     private int m; // cost
     private int[][] opt;
 
@@ -27,11 +30,14 @@ public class Basic {
         double startTime = getTimeInMilliseconds();
 
         basic.setOPT();
+        basic.findAlignments();
 
         double afterUsedMem = getMemoryInKB();
         double endTime = getTimeInMilliseconds();
         double totalUsage =  afterUsedMem-beforeUsedMem;
         double totalTime =  endTime - startTime;
+
+        basic.writeOutput(outputPath, (float) totalTime, (float) totalUsage);
 
         System.out.println("CSCI 570 Project " + basic.getSequence1() + " " + basic.getSequence2());
     }
@@ -42,6 +48,8 @@ public class Basic {
     public Basic() {
         s1 = "";
         s2 = "";
+        a1 = "";
+        a2 = "";
         m = 0;
         opt = new int[s1.length()][s2.length()];
     }
@@ -54,6 +62,8 @@ public class Basic {
     public Basic(String input) {
         s1 = "";
         s2 = "";
+        a1 = "";
+        a2 = "";
         m = 0;
         generateSequences(input);
         opt = new int[s1.length()][s2.length()];
@@ -73,12 +83,85 @@ public class Basic {
         // recurrence
         for (int i = 1; i < opt.length; i++) {
             for (int j = 1; j < opt[0].length; j++) {
+                /** 
+                // x_m is not matched
+                if (GAP_PENALTY + opt[i - 1][j] < MISMATCH_PENALTY[SEQUENCE_INDEX.indexOf(s1.charAt(i))][SEQUENCE_INDEX
+                .indexOf(s2.charAt(j))] + opt[i - 1][j - 1] && GAP_PENALTY + opt[i - 1][j] < GAP_PENALTY + opt[i][j - 1])
+                {
+                    a1 = a1.substring(0, i) + "_" + a1.substring(i);
+                }
+                // y_n is not matched
+                else if (GAP_PENALTY + opt[i][j - 1] < MISMATCH_PENALTY[SEQUENCE_INDEX.indexOf(s1.charAt(i))][SEQUENCE_INDEX
+                .indexOf(s2.charAt(j))] + opt[i - 1][j - 1] && GAP_PENALTY + opt[i][j - 1] < GAP_PENALTY + opt[i - 1][j])
+                {
+                    a2 = a2.substring(0, j) + "_" + a2.substring(j);
+                }
+                */
+
                 opt[i][j] = Math.min(
                         Math.min(MISMATCH_PENALTY[SEQUENCE_INDEX.indexOf(s1.charAt(i))][SEQUENCE_INDEX
                                 .indexOf(s2.charAt(j))] + opt[i - 1][j - 1], GAP_PENALTY + opt[i - 1][j]),
                         GAP_PENALTY + opt[i][j - 1]);
             }
         }
+
+        m = opt[s1.length()-1][s2.length()-1];
+    }
+
+    public void findAlignments()
+    {
+        // top down pass
+        int i = opt.length-1;
+        int j = opt[0].length-1;
+
+        while (i >= 1 && j >= 1)
+        {
+            // x_m
+            if (opt[i-1][j] <= opt[i-1][j-1] && opt[i-1][j] <= opt[i][j-1])
+            {
+                a1 = "_" + s1.charAt(i) + a1;
+                a2 = s2.charAt(j) + a2;
+                i--;
+            }
+            // y_n
+            else if (opt[i][j-1] <= opt[i-1][j-1] && opt[i][j-1] <= opt[i-1][j])
+            {
+                a1 = s1.charAt(i) + a1;
+                a2 = "_" + s2.charAt(j) + a2;
+                j--;
+            }
+            // diagonal
+            else
+            {
+                a1 = s1.charAt(i) + a1;
+                a2 = s2.charAt(j) + a2;
+                i--;
+                j--;
+            }
+        }
+
+        /**
+        for (int i = opt.length-1; i >= 1; i--)
+        {
+            for (int j = opt[0].length-1; j >= 1; j--)
+            {
+                // x_m
+                if (opt[i-1][j] <= opt[i-1][j-1] && opt[i-1][j] <= opt[i][j-1])
+                {
+                    a1 = "_" + a1;
+                }
+                else if (opt[i][j-1] <= opt[i-1][j-1] && opt[i][j-1] <= opt[i-1][j])
+                {
+                    a2 = "_" + a2;
+                }
+                else
+                {
+                    a1 = s1.charAt(i) + a1;
+                    a2 = s2.charAt(j) + a2;
+                }
+            }
+        }
+        */
     }
 
     /**
@@ -131,17 +214,32 @@ public class Basic {
 
     /**
      * Writes the output file
+     * https://www.w3schools.com/java/java_files_create.asp
+     * @param output the output file path
      */
-    private void writeOutput()
+    private void writeOutput(String output, float time, float memory)
     {
         try 
         {
-            File myObj = new File("filename.txt");
-            if (myObj.createNewFile()) {
+            File myObj = new File(output);
+            if (myObj.createNewFile()) 
+            {
               System.out.println("File created: " + myObj.getName());
-            } else {
+            } 
+            else
+            {
               System.out.println("File already exists.");
             }
+
+            FileWriter myWriter = new FileWriter(output);
+            myWriter.write(m + "\n");
+            myWriter.write(a1 + "\n");
+            myWriter.write(a2 + "\n");
+            myWriter.write(Float.toString(time) + "\n");
+            myWriter.write(Float.toString(memory));
+
+            myWriter.close();
+            System.out.println("Successfully wrote to the file.");
         } 
         catch (IOException e) 
         {
